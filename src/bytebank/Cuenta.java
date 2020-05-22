33bdd4b30072455d30393e4b99dd6d2cfbfcea2b
Diego Rojas <darguelles.rojas91@gmail.com>
2020-05-21 22:50:10 -0300

    Clase 6

--- src/bytebank/Cuenta.java
+++ src/bytebank/Cuenta.java
@@ -3,9 +3,19 @@ package bytebank;
 public class Cuenta {
 
     private double saldo;
-    private int agencia;
+    private int agencia = 1;
     private int numero;
-    private Cliente titular;
+    private Cliente titular = new Cliente();
+
+    private static int total;
+
+    public Cuenta( int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("Estoy creando una cuenta " + this.numero);
+
+        Cuenta.total ++;
+    }
 
     public void deposita(double valor) {
         this.saldo = this.saldo + valor;
@@ -39,7 +49,9 @@ public class Cuenta {
     }
 
     public void setAgencia(int agencia) {
-        this.agencia = agencia;
+        if (agencia > 0) {
+            this.agencia = agencia;
+        }
     }
 
     public int getNumero() {
@@ -47,7 +59,9 @@ public class Cuenta {
     }
 
     public void setNumero(int numero) {
-        this.numero = numero;
+        if (numero > 0) {
+            this.numero = numero;
+        }
     }
 
     public Cliente getTitular() {
@@ -56,6 +70,10 @@ public class Cuenta {
 
     public void setTitular(Cliente titular) {
         this.titular = titular;
+    }
+
+    public static int getTotal() {
+        return Cuenta.total;
     }
 
 }
