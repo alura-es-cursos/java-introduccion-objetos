69a4e6b08db20cf0eb563a0b6ccf153019e8cbaf
Diego Rojas <darguelles.rojas91@gmail.com>
2020-05-21 22:14:23 -0300

    Clase 3

--- src/bytebank/Cuenta.java
+++ src/bytebank/Cuenta.java
@@ -7,4 +7,28 @@ public class Cuenta {
     int numero;
     String titular;
 
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfiere(double valor, Cuenta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+
 }
